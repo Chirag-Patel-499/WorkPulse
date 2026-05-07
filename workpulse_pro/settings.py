bb712e4c92f53a42849faaab4d8dc9422af9414d
Chirag Patel <chiragpatel499@gmail.com>
2026-05-07 14:27:40 +0530

    debug

--- workpulse_pro/settings.py
+++ workpulse_pro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3xoers%)n0-e7(%h0xej7=u)#vpqrybvd)n@knab%sif4wyet'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['157.173.218.111']
 
