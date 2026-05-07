2932a34bd85b2d8c9e745a201d82704e304bb903
Chirag Patel <chiragpatel499@gmail.com>
2026-05-07 14:20:30 +0530

    ip set

--- workpulse_pro/settings.py
+++ workpulse_pro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3xoers%)n0-e7(%h0xej7=u)#vpqrybvd)n@knab%sif4wyet'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['157.173.218.111']
 
 
 # Application definition
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'workpulse_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'workpulse',
+        'USER': 'workpulse',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -132,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
